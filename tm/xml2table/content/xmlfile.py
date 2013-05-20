96657301ec01d13b24fa1aa67231ca54845ba585
vietdt <vietdt@gmail.com>
2013-05-21 02:34:12 +0700

    add a blob file field

--- tm/xml2table/content/xmlfile.py
+++ tm/xml2table/content/xmlfile.py
@@ -7,14 +7,37 @@ from Products.Archetypes import atapi
 from Products.ATContentTypes.content import base
 from Products.ATContentTypes.content import schemata
 
+from Products.validation import V_REQUIRED
+from plone.app.blob.field import BlobField
+
 # -*- Message Factory Imported Here -*-
+
+from Products.CMFPlone import PloneMessageFactory as _
 
 from tm.xml2table.interfaces import IXMLFile
 from tm.xml2table.config import PROJECTNAME
+from tm.xml2table import validators
 
 XMLFileSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
 
     # -*- Your Archetypes field definitions here ... -*-
+
+    BlobField('file',
+            required = True,
+            primary = True,
+            searchable = False,
+            accessor = 'getFile',
+            mutator = 'setFile',
+            index_method = 'getIndexValue',
+            languageIndependent = True,
+            storage = atapi.AnnotationStorage(migrate=True),
+            default_content_type = 'application/octet-stream',
+            validators = (('isNonEmptyFile', V_REQUIRED),
+                          ('checkFileMaxSize', V_REQUIRED),
+                          ('isXMLFile', V_REQUIRED)),
+            widget = atapi.FileWidget(label = _(u'label_file', default=u'File'),
+                                description=_(u''),
+                                show_content_type = False,)),
 
 ))
 
