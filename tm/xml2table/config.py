73a8aefe59c32fe55486312e4041ded1c308df36
vietdt <vietdt@gmail.com>
2013-06-06 00:40:03 +0700

    pre-define folders path

--- tm/xml2table/config.py
+++ tm/xml2table/config.py
@@ -7,3 +7,8 @@ ADD_PERMISSIONS = {
     # -*- extra stuff goes here -*-
     'XMLFile': 'tm.xml2table: Add XMLFile',
 }
+
+# pre-defined xml/xsl directories
+TABLE_FOLDER = 'docent/xml2table'
+XML_FOLDER = 'docent/xml2table/uploaded'
+XSL_FOLDER = 'docent/xml2table/xsl-files'
